4c732b2e2840a2a6f089042d1ad778e48ad02dca
rogerioreismuniz <rmuniz.dev@gmail.com>
2021-11-09 13:22:56 -0300

    respons

--- cesta.js
+++ cesta.js
@@ -3,21 +3,24 @@ function addcesta(idbotao) {
         itenssalvos = []
         carrinho = []
     }
-
+    var car = document.getElementById("carrinho")
     var idhtml = idbotao.replace("btn", "iten")
     var itencar = document.getElementById(idhtml)
     var id = parseInt(document.getElementById(idbotao).value)
     var listagem = document.getElementById("listagem")
+    listagem.innerHTML = ""
     itenssalvos.push(itencar)
     carrinho.push({
-            id: id,
-            Nome: itencar
-        })
-        //console.log(itenssalvos[0].children[1].textContent)
+        id: id,
+        Nome: itencar
+    })
+    car.innerHTML = carrinho.length
+
     for (var x = 0; x < itenssalvos.length; x++) {
         var test = document.createElement("p")
         test.innerHTML += carrinho[x].Nome.children[1].textContent
         listagem.appendChild(test)
     }
-
+    alert(`Item adicionado!`)
 }
+const newLocal = $('.carousel').carousel()
